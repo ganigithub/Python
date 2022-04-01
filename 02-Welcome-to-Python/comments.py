2b01b2621c321cf912dc287f807f89acff604543
ganigithub <86476881+ganigithub@users.noreply.github.com>
2022-04-01 12:51:35 +0530

    Add files via upload

---
+++ 02-Welcome-to-Python/comments.py
@@ -0,0 +1,9 @@
+#comments (#)
+
+print('good morning')
+#print('good afternoon') #commenting on a line is ignored by python
+print('good night')
+
+# print(1+2) #select lines nd use ctrl + / to comment multiple lines 
+# print(1+3)
+# print(2+4)
---
+++ 02-Welcome-to-Python/objects-strings.py
@@ -0,0 +1,16 @@
+# strings 
+
+#using single inverted commas
+print('hello world')
+#using double inverted commas
+print("hello world")
+
+#if there is double inv commas in string used single inv commas at start nd end of string 
+print(' he is my "hero"')
+# vice versa
+print(" his card's are good")
+
+# if u want to break sentence or sentence to be printed in next line in output use 3 double inv commas
+print("""i am going 
+to learn python3
+online""")
