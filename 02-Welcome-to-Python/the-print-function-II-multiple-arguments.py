eb86e0940c14b64b02236cf2c144f1862bac05c3
ganigithub <86476881+ganigithub@users.noreply.github.com>
2022-04-01 12:53:09 +0530

    Add files via upload

---
+++ 02-Welcome-to-Python/the-print-function-II-multiple-arguments.py
@@ -0,0 +1,8 @@
+#multiple arguments/inputs
+#comma acts as seperator between arguments and automatically adds space bet them
+print("A", "B", "C")
+print("a","b")
+
+print('hello',5,6,4.234)
+print(4+5, 5-4, 'hello')
+print('my weight is',str(58.5) +'kg')
