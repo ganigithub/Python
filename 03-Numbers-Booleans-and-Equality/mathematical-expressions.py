cbc2dcdb2b4938fd46ff41715866853d9bc7cbd9
ganigithub <86476881+ganigithub@users.noreply.github.com>
2022-04-03 23:28:27 +0530

    Add files via upload

---
+++ 03-Numbers-Booleans-and-Equality/mathematical-expressions.py
@@ -0,0 +1,17 @@
+#Operators
+print(4+3)
+print(4.1 + 9.1)
+print(4.9 - 4.2)
+print(4 * 2.3)
+print(22/7)
+
+print(4 * 'hi')
+
+print(5 ** 3)
+print(3 ** 4)
+
+#PEMDAS(bracket, exponents, mult, div, add, subt)
+print(5 + 5 * 3)
+print((5+5) * 3)
+print(6 * 3 / 3) #if Multiply is first, u can do multiply of division first our choice
+print (9 / 3 * 4**3) #if division is first, u have to do division first
