aedd534762d9846780aa53dd83b145c7113dca39
ganigithub <86476881+ganigithub@users.noreply.github.com>
2022-04-03 23:27:36 +0530

    Add files via upload

---
+++ 03-Numbers-Booleans-and-Equality/division-floor-division-and-the-modulo-operator.py
@@ -0,0 +1,10 @@
+#floor Division '//'(removes fractional part of quotient)
+print (7//3)
+print(6//56) #floor div will be 0 if left operant is less than right
+
+print(-7//3) #if one of operand is -ve, then floor div will be -ve value
+# away from 0 ie. for -2.33 it will be -3 and for -4.65 it will be -5
+
+#modulo/remainder operator '%' (calculates remainder)
+print(7%3)
+print(3%3)
