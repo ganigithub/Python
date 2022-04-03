05dd13db72c56065b48e2378020e71a62322c6ad
ganigithub <86476881+ganigithub@users.noreply.github.com>
2022-04-03 23:29:18 +0530

    Add files via upload

---
+++ 03-Numbers-Booleans-and-Equality/the-type-function.py
@@ -0,0 +1,19 @@
+#Type function : tells what class or blueprint an object belongs to
+print(type(2))
+print(type(9))
+
+print(type(2.0))
+print(type(9.3))
+
+print(type('computer'))
+print(type('laptop'))
+
+print(type(True))
+print(type(False))
+
+print(type(2) == type (9))
+print(type(2.0) == type(4))
+print(type('hello') == type(23))
+print(type('computer') == type('laptop'))
+
+print(type(3 <= 3))
