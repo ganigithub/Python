03ffdb3391ccb75c5b635922ce1af5ad4a6d6481
ganigithub <86476881+ganigithub@users.noreply.github.com>
2022-04-03 23:30:13 +0530

    Add files via upload

---
+++ 03-Numbers-Booleans-and-Equality/type-conversion-with-the-int-float-and-str-functions.py
@@ -0,0 +1,17 @@
+#conversion of arguments from one class to other
+print(int(3))
+print(int(9.3)) #removes fractional part like floor division
+print(int('89'))
+print(int(3/2)) 
+
+print(float(13))
+print(float(13.88))
+print(float('10.44'))
+print(float(3/2))
+
+print(str(3.2))
+print(str(5))
+print('hello' + str(5))
+
+#mixed type conversion
+print(3 + 43.9) #here python automatically converst 3 into float and calculates
