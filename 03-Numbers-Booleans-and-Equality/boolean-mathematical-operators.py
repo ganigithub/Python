1bab9f463d884daf39a01337934ab9fab298c550
ganigithub <86476881+ganigithub@users.noreply.github.com>
2022-04-03 23:26:55 +0530

    Add files via upload

---
+++ 03-Numbers-Booleans-and-Equality/boolean-mathematical-operators.py
@@ -0,0 +1,22 @@
+#comparision operators
+# less than <
+print(5 < 9)
+print(10 < 5)
+
+# less than equal to <=
+print(8 <= 10)
+print(8 <= 8)
+print(8 <= 6)
+
+# greater than >
+print(3 > 2)
+print(5 > 10)
+
+# greater than equal to >=
+print(9 >= 6)
+print(19 >= 100)
+
+# we can check the range 
+print(4 < 9 <= 10)
+print(23 < 999 >= 500)
+print(234 < 9 <= 2389 >238)
