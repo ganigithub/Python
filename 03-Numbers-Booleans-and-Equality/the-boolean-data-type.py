af96733e737c8daacee4e1d34cb9d2cc7eec811d
ganigithub <86476881+ganigithub@users.noreply.github.com>
2022-04-03 23:28:50 +0530

    Add files via upload

---
+++ 03-Numbers-Booleans-and-Equality/the-boolean-data-type.py
@@ -0,0 +1,20 @@
+#Equality operator ==
+print('5' == '5')
+print('hi' == 5) # string is not equal to integer
+print(5 == 5.0)  # float and integer can be compared
+
+#case sensitivity
+print('hi' == 'hi')
+print('hi' == 'Hi')
+
+print(True == True)
+print(False == False)
+print(False == True)
+
+#inequality operator !=
+print(5 != 5)
+print(5 != 6)
+print('hi' != 'hi')
+print('yes' != 'no')
+print('music' != 'noise')
+print(4.0 != 4)
