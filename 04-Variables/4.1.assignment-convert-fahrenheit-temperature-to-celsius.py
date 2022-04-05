7e71f6fd952ffa9bc15614f8286597c9ad24d5e3
ganigithub <86476881+ganigithub@users.noreply.github.com>
2022-04-05 11:39:26 +0530

    Add files via upload

---
+++ 04-Variables/4.1.assignment-convert-fahrenheit-temperature-to-celsius.py
@@ -0,0 +1,11 @@
+# 5 Program to convert temperature from fahrenheit to celsius:
+
+Temperature_in_Faranheit = float(input('what is your temp in F? '))
+print(float((Temperature_in_Faranheit - 32) * (5/9)))
+
+#OR
+
+F = float(input('what is ur temp is F? '))
+print('ur temp in F is', F)
+C = float((F - 32)* (5/9))  
+print('ur temp in C is', C)
