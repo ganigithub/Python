41ee2443ac5f5230b9969c5fef3ec131a65d6783
ganigithub <86476881+ganigithub@users.noreply.github.com>
2022-04-05 11:38:49 +0530

    Add files via upload

---
+++ 04-Variables/2 multiple-variable-assignments.py
@@ -0,0 +1,20 @@
+# 2 
+a = 3
+b = 3
+print(a)
+print(b)
+
+a = b = 1  # this doesnt mean a = b , it means a and b are assigned to 1
+
+b = 10
+print(b)
+print(a)
+
+a, b = 3 , 4
+print(a + b)
+
+a,b,c = 22, 4 ,7
+print(a)
+print(b)
+print(c)
+print('I am',a+b,'years old','and that is', bool(a>c))
