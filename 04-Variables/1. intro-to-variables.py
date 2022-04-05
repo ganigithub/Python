02d3aa7c44f487206499cb0227188ef3521d662b
ganigithub <86476881+ganigithub@users.noreply.github.com>
2022-04-05 11:38:32 +0530

    Add files via upload

---
+++ 04-Variables/1. intro-to-variables.py
@@ -0,0 +1,18 @@
+# 1 variables
+name = 'gani'
+age = -22
+favourite_language = 'python' # snake case notation
+handsome = False
+
+print(name)
+print(handsome)
+print(age)
+print(favourite_language)
+
+print(age + 10)
+print('my name is', name, 'and i am', age, 'years old' + '.')
+age = 40
+print(age + 10)
+
+fact_or_fiction = 10 < 20
+print(fact_or_fiction)
