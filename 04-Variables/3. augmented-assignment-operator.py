fef768c444170557975b311aed6e16e06f3fcb9d
ganigithub <86476881+ganigithub@users.noreply.github.com>
2022-04-05 11:39:07 +0530

    Add files via upload

---
+++ 04-Variables/3. augmented-assignment-operator.py
@@ -0,0 +1,34 @@
+# 3 augmented assignment operator
+a = 3
+a = a + 2
+print(a)
+#another way to do above part is to use an augmented assignment operator
+a = 3
+a += 2
+print(a)
+
+a = 5
+a -= 3
+print(a)
+
+a= 9
+a *= 3
+print(a)
+print(float(a))
+
+a = 5
+a /= 2
+print(a)
+print(int(a))
+
+word = 'car'
+word += 'race'
+print(word)
+
+a = 20
+a %= 3
+print(a)
+
+a = 20
+a //= 3
+print(a)
