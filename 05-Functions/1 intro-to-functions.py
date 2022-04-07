2de906dfd63f6a5598051905426ca5c41b93e46d
Ganesh Nagarvani <ganeshnagarvani@gmail.com>
2022-04-07 11:35:12 +0530

    Add files via upload

---
+++ 05-Functions/1 intro-to-functions.py
@@ -0,0 +1,8 @@
+# 1 intro to funcitons
+
+def first_name():
+    print('Hi I am gani!')        #line 4, 5 and 6 are called body/ block of function
+    print('I am learning python')         #the space before print is called indentation
+    print('I hope to complete program!')
+
+first_name()
