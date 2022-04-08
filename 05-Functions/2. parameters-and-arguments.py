64abf6accf4416c2131126ac04422785b15fad7f
Ganesh Nagarvani <ganeshnagarvani@gmail.com>
2022-04-08 05:48:54 +0530

    Add files via upload

---
+++ 05-Functions/2. parameters-and-arguments.py
@@ -0,0 +1,14 @@
+# 2. Parameters and arguments
+
+def p(Text):
+    print(Text)
+
+p('hi')
+p('hello')
+p('my name is gani!')
+
+def add(a, b):
+    print('addition of', a, '&', b, 'is', a + b)
+
+add(3, 4)
+add(9, -32.542)
