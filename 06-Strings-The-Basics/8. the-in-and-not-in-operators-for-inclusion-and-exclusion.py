1353102f99a4ae148637405754d7b19cea82f629
Ganesh Nagarvani <ganeshnagarvani@gmail.com>
2022-04-19 14:48:11 +0530

    Add files via upload

---
+++ 06-Strings-The-Basics/8. the-in-and-not-in-operators-for-inclusion-and-exclusion.py
@@ -0,0 +1,15 @@
+# 8. in and not in Operators:
+sentence = 'My name is gani.'
+# the in operator checks whether a string is in the original string or not. Return is a bool
+print('gani' in sentence) 
+print('Gani' in sentence)
+print(' ' in sentence)
+print('garry' in sentence)
+
+print()
+
+#the not in operator checks whther a string is not in original string
+print('gani' not in sentence)
+print('Gani' not in sentence)
+print('garry' not in sentence)
+print(' ' not in sentence)
