4ab1b3a4a0ed357b8f0b7c5bb4c60c77aa2798fb
Ganesh Nagarvani <ganeshnagarvani@gmail.com>
2022-05-14 19:32:01 +0530

    Add files via upload

---
+++ 07-Strings-Methods/9. formatted-string-literals.py
@@ -0,0 +1,13 @@
+# 9. Formatted string literals
+
+name = 'garry'
+adj = 'kicked'
+noun = 'deepthi'
+
+sentence = f'{name} was with {adj} {noun}'
+print(sentence)
+
+sentence = F'{name} was with {adj} {noun}'
+print(sentence)
+
+print(f'2 + 2 is {2 + 2}')
