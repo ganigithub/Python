e64cad0ba3453998a95fd1ddbb4eaac770c60c9b
Ganesh Nagarvani <ganeshnagarvani@gmail.com>
2022-05-20 11:03:43 +0530

    Add files via upload

---
+++ 08-Control-Flow/2. the-if-statement.py
@@ -0,0 +1,25 @@
+#2. if statement: body is executed only if condition is True (conditional logic)
+
+if 4>3:                          #if its True, body will be executed
+    print('yes that is True!')
+
+if 4<3:                          #if its False, body will not be executed
+    print('thats not going to print coz its False')
+
+if 'garry' == 'garry':
+    print('nice name!')
+
+if 'Garry' == 'garry':
+    print('will not print')
+
+if 'Garry' != 'garry':
+    print('Garry is not equal to garry')
+
+if True:
+    print('yeah True')
+
+if False:
+    print('thats a lie!')
+
+if 3<4 != True:              #here 3<4 is True then, T
+    print('i hacked u')
