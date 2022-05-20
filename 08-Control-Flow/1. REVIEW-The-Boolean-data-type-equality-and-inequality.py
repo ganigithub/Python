6e8005c8f84cfa1febed016df282d1472af00559
Ganesh Nagarvani <ganeshnagarvani@gmail.com>
2022-05-20 11:03:03 +0530

    Add files via upload

---
+++ 08-Control-Flow/1. REVIEW-The-Boolean-data-type-equality-and-inequality.py
@@ -0,0 +1,17 @@
+#1. revision: boolean data type
+
+print(2<9)
+print(7>10)
+print(10>=9)
+print(4<=5)
+
+print('xbox' == 'xbox')
+print('xbox' == 'Xbox')
+print( 5 == 5)
+print( 5 == 10)
+
+print(5 != 5)
+print(5 != 10)
+print('garry' != 'garry')
+print(True != True)
+print(True != False)
