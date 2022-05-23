4c1062428b54b035a8b98666005f644b70c919e3
Ganesh Nagarvani <ganeshnagarvani@gmail.com>
2022-05-23 11:45:01 +0530

    Add files via upload

---
+++ 08-Control-Flow/4.the-else-statement.py
@@ -0,0 +1,12 @@
+#3. else statement: executes if 'if statement' is False
+
+if 10 > 20:            #here this is false and thus 'else' function will be executed
+    print('Thats true')
+else:                    
+    print('Thats false')
+
+value = int(input('Enter a number: '))
+if value >= 100:
+    print('Thats more than 100')
+else:
+    print('Thats less than 100')
