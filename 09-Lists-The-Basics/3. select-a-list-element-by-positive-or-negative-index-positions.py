c7847f938b6331fe345cc59a3074d4e697626b39
Ganesh Nagarvani <ganeshnagarvani@gmail.com>
2022-06-14 10:59:04 +0530

    Add files via upload

---
+++ 09-Lists-The-Basics/3. select-a-list-element-by-positive-or-negative-index-positions.py
@@ -0,0 +1,13 @@
+#indexing of elements in list starts from 0
+
+web_browsers = ['Chrome', 'Firefox', 'Edge', 'Opera',]
+print(web_browsers[0]) #this gives first element of list
+print(web_browsers[1])
+print(web_browsers[2])
+print(web_browsers[-1]) #this gives last element of list
+print(web_browsers[-2])
+
+print()
+print(web_browsers[1][0])  #this gives first letter of 2nd element in list
+print(web_browsers[3][4])  #this gives 5th letter of fourth element in list
+print(web_browsers[3][-1])
