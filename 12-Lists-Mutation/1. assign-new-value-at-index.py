c2fbffa13e662b7fc220d7fcc5e52cf9fe2751fe
Ganesh Nagarvani <ganeshnagarvani@gmail.com>
2022-07-17 10:52:42 +0530

    Add files via upload

---
+++ 12-Lists-Mutation/1. assign-new-value-at-index.py
@@ -0,0 +1,15 @@
+# assigning new values at index 
+
+movies = ['Avengers', 'Ultron', 'infinity war', 'endgame']
+
+movies[0] = 'Bat Vs Super'
+print(movies)
+
+movies[2] = 'wonder woman'
+print(movies)
+
+movies[-1] = 'Justice League'
+print(movies)
+
+#we cannot add new element in list: this will raise IndexError
+# movies [4] = 'Snyder cut'
