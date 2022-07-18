d61ea577468470d9a4c5df3e5f9623c9d9ee9e95
Ganesh Nagarvani <ganeshnagarvani@gmail.com>
2022-07-18 11:29:41 +0530

    Add files via upload

---
+++ 12-Lists-Mutation/2. assign-new-values-to-list-slice.py
@@ -0,0 +1,13 @@
+fruits = ['mango', 'banana', 'orange', 'grapes', 'pomogranate']
+
+# fruits[2:4] = ['berry', 'jam']
+# print(fruits)
+
+# fruits[2:4] = ['berry'] #we can add just one element. the slice will be replaced by new element
+# print(fruits)
+
+# fruits[2:4] = ['berry', 'jam', 'apple']
+# print(fruits)
+
+# fruits[-4:-1] = ['apple', 'guava']
+# print(fruits)
