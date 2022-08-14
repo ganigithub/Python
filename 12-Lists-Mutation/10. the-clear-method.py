a2e1497ad6d814309ecc86db7641231bedf0a07d
Ganesh Nagarvani <ganeshnagarvani@gmail.com>
2022-08-14 18:06:32 +0530

    Add files via upload

---
+++ 12-Lists-Mutation/10. the-clear-method.py
@@ -0,0 +1,7 @@
+# clear method: removes all elements in list and return an empty list
+
+fruits = ['mango', 'apple', 'banana', 'guava']
+fruits.clear()  #doesnt accept any arguments
+print(fruits)
+
+#we can use clear method when we want to clear the whole element of list and start off fresh
