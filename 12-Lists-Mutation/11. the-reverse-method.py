8de298eafda71a0b443c72f4ad0b815ecffcff52
Ganesh Nagarvani <ganeshnagarvani@gmail.com>
2022-08-28 14:23:18 +0530

    Add files via upload

---
+++ 12-Lists-Mutation/11. the-reverse-method.py
@@ -0,0 +1,7 @@
+# reverse method: .reverse(): reverses the elements of list
+
+numbers = [1,2,3,4,5]
+numbers.reverse()
+print(numbers)
+
+print(numbers.reverse()) #this returns None. so we shouldn't assign this to a variable
