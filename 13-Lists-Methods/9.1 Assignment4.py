3dd9e2b67c6bd45d8269bd6680837934246757ed
Ganesh Nagarvani <ganeshnagarvani@gmail.com>
2022-10-14 11:18:15 +0530

    Add files via upload

---
+++ 13-Lists-Methods/9.1 Assignment4.py
@@ -0,0 +1,34 @@
+
+# The floats variable should store the floating point values for each string in the values list.
+values = ["3.14", "9.99", "567.324", "5.678"]
+floats = [float(number) for number in values]
+print(floats)
+
+# The letters variable should store a list of 5 strings. 
+# Each of the strings should be a character from name concatenated together 3 times.
+# i.e. ['BBB', 'ooo', 'rrr', 'iii', 'sss']
+name = "Boris"
+letters = [char * 3  for char in name]
+print(letters)
+
+# The 'lengths' list should store a list with the lengths of each string in the 'elements' list
+elements = ["Hydrogen", "Helium", "Lithium", "Boron", "Carbon"]
+lengths = [len(element) for element in elements]
+print(lengths)
+
+
+# Declare a destroy_elements function that accepts two lists.
+# It should return a list of all elements from the first list that are NOT contained in the second list.
+# Use list comprehension in your solution.
+#
+# EXAMPLES
+# destroy_elements([1, 2, 3], [1, 2])      => [3]
+# destroy_elements([1, 2, 3], [1, 2, 3])   => []
+# destroy_elements([1, 2, 3], [4, 5])      => [1, 2, 3]
+
+def destroy_elements(first, second):
+    return [element for element in first if element not in second]
+
+print(destroy_elements([1,2,3], [1,2]))
+print(destroy_elements([1,2,3], [1,2,3]))
+print(destroy_elements([1,2,3], [4,5]))
