be450b55451ca65d12590bb41093a3ffe235924f
Ganesh Nagarvani <ganeshnagarvani@gmail.com>
2022-10-18 11:03:37 +0530

    Add files via upload

---
+++ 14-Built-in-Functions/1. the-help-function.py
@@ -0,0 +1,14 @@
+#help funciton: help(builtin.object):returns the documentation of functions if we want to know about function
+#remember do not invoke it
+
+# help(print)
+# help(len)
+
+# help(int)
+# help(str)
+# help(list)
+
+# help([].extend)
+# help([].index)
+# help('hello'.replace)
+# help('hello'.swapcase)
